e8463b7b5af6cd3c040091b187bd0fe52e913551
Martijn Nahumury <martijnnahumury@yahoo.com>
2016-03-16 14:01:38 -0500

    Initial Commit

---
+++ Twitterizer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  Twitterizer
+//
+//  Created by Martijn Nahumury on 16/03/16.
+//  Copyright © 2016 MFN Advies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ Twitterizer/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Twitterizer
+//
+//  Created by Martijn Nahumury on 16/03/16.
+//  Copyright © 2016 MFN Advies. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
